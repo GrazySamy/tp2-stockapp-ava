b3b38fcf9522b3e74d535702df7365e507ddf5a7
Samanta Graziela <samanta.graziela@gmail.com>
2024-06-23 18:56:07 -0300

    Configuração do Swagger para Usar JWT

--- StockApp.API/Controllers/CategoriesController.cs
+++ StockApp.API/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using StockApp.Application.DTOs;
 using StockApp.Application.Interfaces;
 
@@ -6,6 +7,7 @@ namespace StockApp.API.Controllers
 {
     [Route("/api/[controller]")]
     [ApiController]
+    [Authorize]
     public class CategoriesController : ControllerBase
     {
         private readonly ICategoryService _categoryService;
